f6353c03f6ce47169ca949722611bb2a95ac3224
SaiPranav Madupu <msaipranav2004@gmail.com>
2025-05-06 20:58:34 +0530

    added quantization

--- src/utils/model_runner.py
+++ src/utils/model_runner.py
@@ -4,7 +4,7 @@
     This module contains functions to load and run a pre-trained model.
 """
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import sys
 import os
 
@@ -14,13 +14,22 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.
 from src.utils.load_config import load_llm_config
 
 model_config = load_llm_config()
+
+# Define quantization config
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype="float16",
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+)
+
 llm_model_name = model_config["provider"] + "/" + model_config["model"]
 
-llm_tokenizer = AutoTokenizer.from_pretrained(llm_model_name, trust_remote_code=True)
+llm_tokenizer = AutoTokenizer.from_pretrained(llm_model_name)
 llm_tokenizer.pad_token = llm_tokenizer.eos_token
 llm_tokenizer.padding_side = "right"
 
-llm_model = AutoModelForCausalLM.from_pretrained(llm_model_name, trust_remote_code=True)
+llm_model = AutoModelForCausalLM.from_pretrained(llm_model_name, quantization_config=bnb_config)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 llm_model.to(device)
 
